71528678e4415793766e008dc9f528ce4c163d63
NM <noil.oomman@yahoo.com>
2021-05-10 07:46:38 +1000

    your commit message here

---
+++ aws_terraform/output.tf
@@ -0,0 +1,18 @@
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "elb_web_dns" {
+  description = "External Load Balancer"
+  value       = "${aws_lb.web_app_lb.dns_name}"
+}
+
+output "elb_app_dns" {
+  description = "Internal Load Balancer"
+  value       = "${aws_lb.appl_app_lb.dns_name}"
+}
+
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = "${aws_db_instance.db_postgres.endpoint}"
+}
---
+++ aws_terraform/variables.tf
@@ -0,0 +1,29 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_region" {
+    description = "AWS Region"
+    type    = string
+}
+
+variable "vpc_cidr_block" {
+    description = "Main VPC CIDR Block"
+    type    = string
+}
+
+variable "az_public_subnet" {
+  type = map(string)
+}
+
+variable "az_private_subnet" {
+  type = map(string)
+}
+
+variable "az_database_subnet" {
+  type = map(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
