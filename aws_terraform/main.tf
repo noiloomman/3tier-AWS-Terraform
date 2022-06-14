1953e2ac6c28667574c87b1a3fe103fa553a6d71
noiloomman <52391654+noiloomman@users.noreply.github.com>
2022-06-14 14:27:12 +1000

    Update main.tf
    updated Terraform Version

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -1,8 +1,17 @@
+######terraform {
+ ##### required_providers {
+    ####aws = {
+      ###source  = "hashicorp/aws"
+      ###version = "~> 3.26.0"
+   # }
+ # }
+#}
 terraform {
+  required_version = "~> 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      #version = "~> 3.26.0"
+      version = "~> 3.0"
     }
   }
 }
