78d9e3906a4fd6ce9454f2c809c3cddc1bbd538e
Noil Oomman <noil.oomman@yahoo.com>
2022-06-15 12:40:50 +1000

    Updated output file for Interpolation issue"

--- aws_terraform/output.tf
+++ aws_terraform/output.tf
@@ -4,15 +4,15 @@
 
 output "elb_web_dns" {
   description = "External Load Balancer"
-  value       = "$aws_lb.web_app_lb.dns_name"
+  value       = aws_lb.web_app_lb.dns_name
 }
 
 output "elb_app_dns" {
   description = "Internal Load Balancer"
-  value       = "$aws_lb.appl_app_lb.dns_name"
+  value       = aws_lb.appl_app_lb.dns_name
 }
 
 output "rds_endpoint" {
   description = "RDS endpoint"
-  value       = "$aws_db_instance.db_postgres.endpoint"
+  value       = aws_db_instance.db_postgres.endpoint
 }
