7041dfd35058079471333981c48ac8eeb2d004e8
Noil Oomman <noil.oomman@yahoo.com>
2022-06-15 16:05:16 +1000

    Updated output file for Interpolation issue"

--- aws_terraform/variables.tf
+++ aws_terraform/variables.tf
@@ -4,24 +4,24 @@
 
 variable "aws_region" {
     description = "AWS Region"
-    type    = list(string)
+    type    = (string)
 }
 
 variable "vpc_cidr_block" {
     description = "Main VPC CIDR Block"
-    type    = list(string)
+    type    = (string)
 }
 
 variable "az_public_subnet" {
-  type = map(string)
+  type = list(string)
 }
 
 variable "az_private_subnet" {
-  type = map(string)
+  type = list(string)
 }
 
 variable "az_database_subnet" {
-  type = map(string)
+  type = list(string)
 }
 
 variable "availability_zone" {
